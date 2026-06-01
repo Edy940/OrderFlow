442eb2a1ae799e779054bdac064dcbd8232bcdf8
EdWander <edwandersilva@gmail.com>
2026-06-01 08:45:48 -0300

    feat: adiciona DTOs de resposta para pedidos

---
+++ src/Orderflow.Application/DTO/ItemPedidoResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderFlow.Application.DTO
+{
+    public class ItemPedidoResponseDto
+    {
+        public string Produto { get; set; }
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
